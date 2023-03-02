687403fb258f6949b35f38e353527df97e1db62b
pankaj <adhikarisinghpankaj231@gmail.com>
2023-03-03 01:08:51 +0530

    first

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -25,5 +25,43 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+            if(oldPassword.equals(password)){
+                if(isValid(newPassword)){
+                    System.out.println("Password changed successfully");
+                }else{
+                    System.out.println("The new password is not valid");
+                }
+            }else{
+                System.out.println("The given password does not match the current password!");
+            }
+    }
+    public boolean isValid(String password){
+        boolean capitals = false;
+        boolean smalls = false;
+//        int charcount = 0;
+        boolean isdigit = false;
+        boolean special = false;
+
+        if(password.length() < 8){
+            return false;
+        }
+
+        for(int i = 0; i<password.length(); i++){
+            char x = password.charAt(i);
+
+            if((x >= 'A') && (x <= 'Z')){
+                capitals = true;
+            } else if ((x >= 'a') && (x <= 'z')) {
+                smalls = true;
+            } else if ((x >= '0') && (x <= '9')) {
+                isdigit = true;
+            }else {
+                special = true;
+            }
+        }
+
+        if(capitals && smalls && isdigit && special){
+            return true;
+        }else return false;
     }
 }
